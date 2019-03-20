72a1dcb9881a696350884d2f74cfe9443481f41b
hughes28 <jfhughes28@gmail.com>
2019-03-19 20:19:42 -0700

    created PositionTracker (which tracks position of object) and OpenDoor (which opens a door)

--- BuildingEscape/Source/BuildingEscape/PositionReport.cpp
+++ BuildingEscape/Source/BuildingEscape/PositionReport.cpp
@@ -21,7 +21,8 @@ void UPositionReport::BeginPlay()
 	Super::BeginPlay();
 
 	FString ObjectName = GetOwner()->GetName();
-	UE_LOG(LogTemp, Warning, TEXT("Position report reporting for duty on %s!"), *ObjectName);
+	FVector ObjectPos = GetOwner()->GetTransform().GetLocation();
+	UE_LOG(LogTemp, Warning, TEXT("%s is at %s"), *ObjectName, *ObjectPos.ToString());
 	
 }
 
